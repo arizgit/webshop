5f4d6c2b377bc147db01a12802434459de8f54e4
Ariz Borcelis <>
2023-09-20 19:06:25 +0800

    add new page file for register URL and its form component

---
+++ src/app/register/page.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import RegisterForm from '../../components/registerForm'
+import type { Metadata } from 'next'
+export const metadata: Metadata = {
+  title: 'Register - Webshop',
+  description: 'Built with Nextjs',
+}
+
+export default function Register() {
+  return (
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
+            Welcome to WebShop!
+          </h1>
+          <h2 class="text-lg font-bold leading-tight tracking-tight md:text-xl text-white">
+            Create an account
+          </h2>
+          <RegisterForm/>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/registerForm.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+
+export default function RegisterForm() {
+  return (
+    <form class="space-y-4 md:space-y-6">
+      <div>
+        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email Address</label>
+        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+      </div>
+      <div>
+        <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+        <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+      </div>
+      <div>
+        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+        <input type="password" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+      </div>
+      <div>
+        <label for="confirm_password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+        <input type="password" name="confirm_password" id="confirm_password" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+      </div>
+      <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-primary-600 hover:bg-primary-700 focus:ring-primary-800">Submit</button>
+      <p class="text-sm font-light text-gray-300">
+        Already have an account? <Link href="/" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign in</Link>
+      </p>
+    </form>
+  )
+}
